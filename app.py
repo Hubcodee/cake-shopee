83f6e21232ea4a0f4ec386cfb79252a13107e7fc
Hubcodee <anshshrivas26@gmail.com>
2021-07-25 15:05:04 +0530

    CRUD operation updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from user_db import db, fetch_db, mod_db
+from user_db import db, fetch_db, mod_db, del_user, del_all
 app = Flask(__name__)
 
 
@@ -30,15 +30,64 @@ def form_details():
                 return(html_code)
             else:
                 return("<h1>404 error</h1>")
+        elif request.form.get("delete"):
+            return render_template("delete.html")
     else:
         return redirect('/')
 
 
-@app.route("/mod", methods=["POST"])
-def mod():
+@app.route("/modification", methods=["POST"])
+def user_mod():
     if request.method == "POST":
-        if request.form.get("update"):
-            pass
+        email = request.form.get("email")
+        # return (str(request.form.get("User_Details")))
+        if str(request.form.get("User_Details")) == "Name":
+            name = request.form.get("in")
+        else:
+            name = None
+        if str(request.form.get("User_Details")) == "Faculty Name":
+            company = request.form.get("in")
+        else:
+            company = None
+        if str(request.form.get("User_Details")) == "Contact Number":
+            contact = request.form.get("in")
+        else:
+            contact = None
+        if str(request.form.get("User_Details")) == "Remarks":
+            message = request.form.get("in")
+        else:
+            message = None
+        res, code = mod_db(name, email, company, contact, message)
+        if code == True:
+            return redirect('/')
+        else:
+            return("<h1>Failed to query !! <a href='output.html'>Go back!</a></h1>")
+    else:
+        return("<h1>Error 404</h1>")
+
+
+@app.route("/delete", methods=["POST"])
+def user_del():
+    if request.method == "POST":
+        if request.form.get("delete"):
+            name = request.form.get("name")
+            email = request.form.get("email")
+            res, code = del_user(name, email)
+            if(code == True):
+                return("<h1 style='color:green'>User deleted successfully <a href='/'>Click to return</a></h1>")
+            else:
+                return("<h1>404 error</h1>")
+        elif request.form.get("fetch"):
+            res, code = fetch_db()
+            if(code == True):
+                html_code = render_template("output.html", res=res)
+                return(html_code)
+            else:
+                return("<h1>404 error</h1>")
+        elif request.form.get("delete_all"):
+            code = del_all()
+            if(code == True):
+                return("<h1 style='color:green'>Your collection is clean <a href='/'>Click to return</a></h1>")
 
 
 if __name__ == '__main__':
--- user_db.py
+++ user_db.py
@@ -20,25 +20,37 @@ def fetch_db():
     return res, True
 
 
-def mod_db(name, email, company, contact, message):
+def mod_db(name=None, email=None, company=None, contact=None, message=None):
     db = "test"
     col = "test"
-    user = {"name": name, "email": email, "company": company,
-            "contact": contact, "message": message}
-    client[db][col].update_one()
+    if name != None:
+        query = {"name": name}
+    elif company != None:
+        query = {"company": company}
+    elif contact != None:
+        query = {"contact": contact}
+    elif message != None:
+        query = {"message": message}
+    user = {"email": email}
+    client[db][col].update(user, {"$set": query})
     res = client[db][col].find()
     return res, True
 
 
-def del_user(name=None, email=None, company=None, contact=None, message=None):
+def del_user(name=None, email=None):
     db = "test"
     col = "test"
-    user = {"name": name, "email": email, "company": company,
-            "contact": contact, "message": message}
-
+    user = {"name": name, "email": email}
     client[db][col].delete_one(user)
     res = client[db][col].find()
     return res, True
+
+
+def del_all():
+    db = "test"
+    col = "test"
+    client[db][col].remove({})
+    return True
 
     # for i in res:
     #     print(i)
